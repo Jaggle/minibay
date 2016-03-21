54c128d1770528ece93caeb8adcab603c2491501
Jake <singviy@qq.com>
2016-03-21 23:36:54 +0800

    add install and data files

---
+++ data/conf/db.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * 配置文件
+ */
+return array(
+    'DB_TYPE' => 'mysql',
+    'DB_HOST' => 'localhost',
+    'DB_NAME' => 'lw',
+    'DB_USER' => 'root',
+    'DB_PWD' => '',
+    'DB_PORT' => '3306',
+    'DB_PREFIX' => 'lw_',
+    //密钥
+    "AUTHCODE" => '9E4X0tybEViVjXyDay',
+    //cookies
+    "COOKIE_PREFIX" => 'tNHR9f_',
+);
---
+++ data/runtime/Data/site_options.php
@@ -0,0 +1,1 @@
+a:10:{s:9:"site_name";s:18:"智能生活助手";s:9:"site_host";s:21:"http://localhost/lun/";s:9:"site_root";s:0:"";s:8:"site_icp";s:0:"";s:16:"site_admin_email";s:14:"singviy@qq.com";s:11:"site_tongji";s:0:"";s:14:"site_copyright";s:0:"";s:14:"site_seo_title";s:18:"智能生活助手";s:17:"site_seo_keywords";s:18:"智能生活助手";s:20:"site_seo_description";s:18:"智能生活助手";}
