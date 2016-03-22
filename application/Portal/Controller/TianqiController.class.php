8af3403361aa2bfddbdc5795efd7c1a844384157
Jake <singviy@qq.com>
2016-03-22 22:55:04 +0800

    no message

---
+++ application/Portal/Controller/TianqiController.class.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace Portal\Controller;
+
+
+use Common\Controller\HomebaseController;
+
+class TianqiController extends HomebaseController
+{
+
+    public function index()
+    {
+        $this->display(":tianqi");
+    }
+
+}
