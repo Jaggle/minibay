e5d2d278a99267bb9a1bed84c2eda2ea1a05000e
Jake <singviy@qq.com>
2016-03-22 23:57:58 +0800

    no message

---
+++ application/Portal/Controller/KuaidiController.class.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace Portal\Controller;
+
+
+use Common\Controller\HomebaseController;
+
+class kuaidiController extends HomebaseController
+{
+
+    public function index()
+    {
+        $this->display(":kuaidi");
+    }
+
+}
