04a79d5ac385585eb51c5202aa2d6e2389fbcfe5
Jake <singviy@qq.com>
2016-03-22 10:05:31 +0800

    add logo

--- data/conf/db.php
+++ data/conf/db.php
@@ -4,7 +4,7 @@
  */
 return array(
     'DB_TYPE' => 'mysql',
-    'DB_HOST' => 'localhost',
+    'DB_HOST' => 'yeskn.com',
     'DB_NAME' => 'lw',
     'DB_USER' => 'admin',
     'DB_PWD' => 'new-password',
